f8f7f3d5c3be81f7a99a75008d2f95b3786d2f01
jaemushim <shimwoan@gmail.com>
2021-08-16 14:15:35 +0900

    무한스크롤

---
+++ src/useBookSearch.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+export default function useBookSearch(query, pageNumber) {
+  const [loading, setloading] = useState(true);
+  const [error, setError] = useState(false);
+  const [books, setBooks] = useState([]);
+  const [hasMore, setHasMore] = useState(false);
+
+  useEffect(() => {
+    setBooks([]);
+  }, [query]);
+  useEffect(() => {
+    setloading(true);
+    setError(false);
+    let cancel;
+    axios({
+      method: 'GET',
+      url: 'http://openlibrary.org/search.json',
+      params: { q: query, page: pageNumber },
+      cancelToken: new axios.CancelToken((c) => (cancel = c)),
+    })
+      .then((res) => {
+        setBooks((prevBooks) => {
+          return [
+            ...new Set([...prevBooks, ...res.data.docs.map((b) => b.title)]),
+          ];
+        });
+        setHasMore(res.data.docs.length > 0);
+        setloading(false);
+      })
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+        setError(true);
+      });
+    return () => cancel();
+  }, [query, pageNumber]);
+
+  return { loading, error, books, hasMore };
+}
